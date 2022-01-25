4d1aaf8da77dd0be3751000fdb6267577dfcb738
Semjon Krolzik <semjon.krolzik@accenture.com>
2022-01-25 16:14:22 +0100

    added security issue

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 	protected static final Logger LOG = LogManager.getLogger();
+	protected String password = "supergeheim";
 
 	@GetMapping("/")
 	public String index() {
-		LOG.info("Called index page in webapp!");
+		LOG.info("Called index page in webapp - with password " + password);
 		return "Greetings from Spring Boot!";
 	}
 
